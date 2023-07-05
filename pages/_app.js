dfa76d88090ccb66a3bc73d456971a81c81752ef
Johannes Osterkamp <johannes.osterkamp@gmx.net>
2023-07-05 11:57:43 +0200

    add SWR fetch and dynamic routing

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,19 @@
 import GlobalStyle from "../styles";
+import { SWRConfig } from "swr";
+
+async function myFetcher(url) {
+  const response = await fetch(url);
+  const data = await response.json();
+  return data;
+}
 
 export default function App({ Component, pageProps }) {
   return (
     <>
       <GlobalStyle />
-      <Component {...pageProps} />
+      <SWRConfig value={{ fetcher: myFetcher }}>
+        <Component {...pageProps} />
+      </SWRConfig>
     </>
   );
 }
